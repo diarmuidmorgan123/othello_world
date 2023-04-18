6d3f6a79af1bace18dcf17c06c5d1f8c3cc64cb3
diarmuidmorgan123 <125451234+diarmuidmorgan123@users.noreply.github.com>
2023-04-18 09:19:22 +0100

    Create make_results.sh

---
+++ make_results.sh
@@ -0,0 +1,37 @@
+#/bin/bash
+python train_probe_othello.py --layer 6 --mid_dim 64 --championship --splitconsec --twolayer
+python train_probe_othello.py --layer 6 --mid_dim 64 --championship --splitconsec --twolayer
+python train_probe_othello.py --layer 6 --mid_dim 64 --championship --splitconsec --twolayer
+python train_probe_othello.py --layer 6 --mid_dim 64 --championship --splitconsec --twolayer
+python train_probe_othello.py --layer 6 --mid_dim 64 --championship --splitconsec --twolayer
+
+python train_probe_othello.py --layer 6 --mid_dim 64 --championship  --twolayer
+python train_probe_othello.py --layer 6 --mid_dim 64 --championship --twolayer
+python train_probe_othello.py --layer 6 --mid_dim 64 --championship --twolayer
+python train_probe_othello.py --layer 6 --mid_dim 64 --championship  --twolayer
+python train_probe_othello.py --layer 6 --mid_dim 64 --championship  --twolayer
+
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship --splitconsec --twolayer
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship --splitconsec --twolayer
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship --splitconsec --twolayer
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship --splitconsec --twolayer
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship --splitconsec --twolayer
+
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship  --twolayer
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship --twolayer
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship --twolayer
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship  --twolayer
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship  --twolayer
+
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship --splitconsec 
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship --splitconsec 
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship --splitconsec 
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship --splitconsec 
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship --splitconsec 
+
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship  
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship 
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship 
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship  
+python train_raw_seq.py --layer 6 --mid_dim 64 --championship 
+
